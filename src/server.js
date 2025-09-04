9456b893caf81502c779019e29085041c8436b14
khantnaythwayzaw <khantnay55555@gmail.com>
2025-09-04 22:23:47 +0630

    Implemented Websocket to sync msg

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
 import app from "./app.js";
 import mongoose from "mongoose";
+import { Server } from "socket.io";
+import User from "./model/userModel.js";
+import Msg from "./model/messageModel.js";
 dotenv.config({ path: "./config.env" });
 
 mongoose
@@ -8,6 +11,28 @@ mongoose
   .then(console.log("DB connection successful"));
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`App running on port `, PORT);
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://127.0.0.1:8080",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("sendMessage", async (text, userId) => {
+    try {
+      const newMsg = await Msg.create({
+        text: text,
+        user: userId,
+      });
+
+      io.emit("receiveMessage", newMsg);
+    } catch (err) {
+      console.log(err);
+    }
+  });
 });
